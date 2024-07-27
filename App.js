0d04d24f3fd3eccd2efc161f150af75cc4a7d68b
Sandu <tharakasanu80@gmail.com>
2024-07-27 23:48:40 +0530

    updated project

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Let's develop pizza appication!</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
 });
---
+++ screens/HomeScreen.js
@@ -0,0 +1,46 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  const data = [
+    {
+      id: "0",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe-n_NBXmFXRWT7iGC-THljTXxeSLHJsV9kg&usqp=CAU",
+    },
+    {
+      id: "1",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtyUHaJAMXYPvV6a_-_XqE0YhhDGpCPofVOQ&usqp=CAU",
+    },
+    {
+      id: "2",
+      image:
+        "https://i0.wp.com/www.promotionsinuae.com/wp-content/uploads/2018/07/Buy-Any-Medium-or-Large-Pizza-Get-The-2nd-Pizza-Free-Order-Online-Only..jpg?fit=552%2C287&ssl=1",
+    },
+    {
+      id: "3",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDEcz7DVk3uBX_33FZVdPjiCvgpKsmx_9eSh7x4GrTyeMsQixOTd7SHXByKmdibQSQF94&usqp=CAU",
+    },
+    {
+      id: "4",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEez8xQZfPxqFsk250_vhXqgRz5Nv8NsHayohTjVexmQwLwhsFUxnElS6bkxLb7gTvHTM&usqp=CAU",
+    },
+    {
+      id: "5",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeyDcZ1Y3GcIx0CB9Qd8MGw5IE6eASPzwWVIs-yklFqqd7O3bfXj5egUmREG0dUK6M6o8&usqp=CAU",
+    },
+  ];
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})

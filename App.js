ed14ca91898f795fbcf7e1bd565ad15436dcb672
Sandu <tharakasanu80@gmail.com>
2024-07-28 14:24:55 +0530

    updated navigator

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
+import StackNavigator from './StackNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-     <HomeScreen/> 
-      <StatusBar style="auto" />
-    </View>
+   <StackNavigator/>
   );   
 }
 

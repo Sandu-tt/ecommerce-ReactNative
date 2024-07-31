457f7e38dfc09f7c46988cce88701339bc6e362a
Sandu <tharakasanu80@gmail.com>
2024-07-31 23:21:33 +0530

    updated pizza page with components

---
+++ components/PizzaComponent.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PizzaComponent = () => {
+  return (
+    <View>
+      <Text>PizzaComponent</Text>
+    </View>
+  )
+}
+
+export default PizzaComponent
+
+const styles = StyleSheet.create({})
--- screens/PizzaScreen.js
+++ screens/PizzaScreen.js
@@ -1,11 +1,22 @@
-import { StyleSheet, Text, View, SafeAreaView  } from 'react-native'
+import { FlatList, SafeAreaView, StyleSheet, Text, View} from 'react-native'
 import React from 'react'
+import pizza from '../data/pizza'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
+import PizzaComponent from '../components/PizzaComponent';
 
 
 const PizzaScreen = () => {
+  const data=pizza;
+  const navigation=useNavigation();
   return (
     <SafeAreaView>
-      <Text>PizzaScreen</Text>
+     <Ionicons  onPress={() =>navigation.goBack()} name="arrow-back-circle" size={24} color="black" />
+     <FlatList data={data} renderItem={({item}) =>(
+<PizzaComponent pizza={item}/>
+     )}
+
+      />
     </SafeAreaView>
   )
 }

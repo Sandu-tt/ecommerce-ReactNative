d21084b08b61dad57fce44cc27edf9b1cd74f1f5
Sandu <tharakasanu80@gmail.com>
2024-08-03 11:03:06 +0530

    updated pizza cart

--- components/PizzaComponent.js
+++ components/PizzaComponent.js
@@ -1,25 +1,65 @@
 import { Pressable, StyleSheet, Text, View ,Image} from 'react-native'
 import React from 'react'
+import ModalDropdown from 'react-native-modal-dropdown';
 
 const PizzaComponent = ({pizza}) => {
     
     const data =[pizza];
    // console.log("data",data);
+   const options=['regular', 'medium', 'large']
+   
   return (
     <View>
       {data.map((item, index)=>(
-        <Pressable>
-           <Image style={{height:200,aspectRatio:5/5,resizeMode:"cover"}} source={{uri:item.image}} />
+        <Pressable style={{ borderEndColor: "#AFD8F5", borderWidth: 0.1 }}>
+           <Image style={{height:200,aspectRatio: 1 / 1,resizeMode:"cover"}}
+            source={{uri:item.image}} 
+            />
            <View style={{backgroundColor:"#006491",padding:10}}>
-            <Text style={{fontSize:17,fontWeight:"bold",color:"white"}}>{item.name.substr(0,15) +"..."}</Text>
+            <Text style={{fontSize:15,fontWeight:"bold",color:"white"}}>
+                {item.name.substr(0,14) + "..."} 
+             </Text>
 
-            <Text style={{color:"pink"}}>{item.description.substr(0,25) + "..."}</Text>
-           </View>
+            <Text style={{color:"pink",marginTop:4}}>
+                {item.description.substr(0,20) + "..."}
 
-           <Pressable>
+            </Text>
 
             
+           <Pressable style={{
+                marginTop:8,
+                flexDirection:"row",
+                alignItems:"center",
+                marginTop: 10
+
+            }}>
+
+                <View>
+                    <Text style={{ color: "white", fontSize: 15 }}>Size</Text>
+                <ModalDropdown dropdownStyle={{width:60,height:100}}  
+                    style={{width:60}}
+                    options={options}>
+                </ModalDropdown>
+
+                </View>
+
+
+               
+
+            
+            <Pressable style={{
+                backgroundColor:"#03C03C",
+                padding:5, 
+                marginLeft: 15,
+                borderRadius: 4,
+            }}>
+                <Text style={{ color: "white", fontWeight: "bold" }}>Add To Cart</Text>
            </Pressable>
+
+         </Pressable>  
+            
+           </View>
+
         </Pressable>
       ))}
     </View>
